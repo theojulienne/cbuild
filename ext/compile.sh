82f4e453f0957a7ec2ffa3d7d7cb2d6d26b8e8dd
Theo Julienne <theo.julienne@gmail.com>
2007-05-12 01:12:22 +0000

    add cbuild-darwin binary, make bash script actually intelligently call it

--- ext/compile.sh
+++ ext/compile.sh
@@ -1,3 +1,24 @@
 #!/bin/sh
 
-echo "cbuild caller for bash"
+OS=`uname`
+TARGET="unknown"
+
+case "$OS" in
+	"Darwin")
+		TARGET="darwin"
+		;;
+	*)
+		echo "Sorry, I don't understand the OS 'uname' returned: $OS"
+		exit 1
+		;;
+esac
+
+BIN=`dirname $0`/binaries/cbuild-$TARGET
+
+if [ ! -e "$BIN" ]; then
+	echo "Could not find pre-compiled cbuild '$BIN'"
+	exit 1
+fi
+
+echo "Launching cbuild for host '$TARGET'"
+$BIN $*
